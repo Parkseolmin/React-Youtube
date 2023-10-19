687b049011583286bb6bb55f118ab57ab743be48
Parkseolmin <qkrtjfals2@naver.com>
2023-10-20 01:36:20 +0900

    추천 영상 작업

---
+++ src/components/contents/Gsap.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Gsap() {
+    return <div>Gsap</div>;
+}
--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { todayText } from '../../data/today.js';
+import { Link } from 'react-router-dom';
 
 export default function Today() {
-    return <div>Today</div>;
+    return (
+        <section id='today'>
+            <div className='today__inner'>
+                <div className='today__thumb play__icon'>
+                    <Link to={todayText[0].page}>
+                        <img src={todayText[0].img} alt={todayText[0].title} />
+                    </Link>
+                </div>
+                {/* //today__thumb */}
+                <div className='today__text'>
+                    <span className='today'>today!</span>
+                    <h3 className='title'>
+                        <Link to={todayText[0].page}>{todayText[0].title}</Link>
+                    </h3>
+                    <p className='desc'>{todayText[0].desc}</p>
+                    <div className='info'>
+                        <span className='author'>
+                            <Link to={`/channel/${todayText[0].channelId}`}>
+                                {todayText[0].author}
+                            </Link>
+                        </span>
+                        <span className='data'>{todayText[0].date}</span>
+                    </div>
+                </div>
+                {/* //today__text */}
+            </div>
+        </section>
+    );
 }
