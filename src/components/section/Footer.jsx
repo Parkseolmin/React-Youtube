368a93068e2a2bfb0acfcf98054aad626e5163ed
Parkseolmin <qkrtjfals2@naver.com>
2023-10-19 19:37:59 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function Footer() {
-  return (
-    <div id='footer' role='contentinfo'>
-      Footer
-    </div>
-  );
+    return (
+        <footer id='footer' role='contentinfo'>
+            <a href='mailto:qkrtjfals2@naver.com' rel='nooperner noreferrer'>
+                qkrtjfals2@naver.com
+            </a>
+        </footer>
+    );
 }
 
 export default Footer;
