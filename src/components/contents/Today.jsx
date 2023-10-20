703389fab9dfc349b4ac959a0ded5c145ee0cf93
Parkseolmin <qkrtjfals2@naver.com>
2023-10-20 17:51:34 +0900

    추천 개발자 작업

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -3,28 +3,29 @@ import { todayText } from '../../data/today.js';
 import { Link } from 'react-router-dom';
 
 export default function Today() {
+    const firstItem = todayText[0];
     return (
         <section id='today'>
             <div className='today__inner'>
                 <div className='today__thumb play__icon'>
-                    <Link to={todayText[0].page}>
-                        <img src={todayText[0].img} alt={todayText[0].title} />
+                    <Link to={firstItem.page}>
+                        <img src={firstItem.img} alt={firstItem.title} />
                     </Link>
                 </div>
                 {/* //today__thumb */}
                 <div className='today__text'>
                     <span className='today'>today!</span>
                     <h3 className='title'>
-                        <Link to={todayText[0].page}>{todayText[0].title}</Link>
+                        <Link to={firstItem.page}>{firstItem.title}</Link>
                     </h3>
-                    <p className='desc'>{todayText[0].desc}</p>
+                    <p className='desc'>{firstItem.desc}</p>
                     <div className='info'>
                         <span className='author'>
-                            <Link to={`/channel/${todayText[0].channelId}`}>
-                                {todayText[0].author}
+                            <Link to={`/channel/${firstItem.channelId}`}>
+                                {firstItem.author}
                             </Link>
                         </span>
-                        <span className='data'>{todayText[0].date}</span>
+                        <span className='data'>{firstItem.date}</span>
                     </div>
                 </div>
                 {/* //today__text */}
